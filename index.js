8e76bfb1b2ef0c34fc9ebd3aeaa301387919f1cd
unknown <you@example.com>
2024-06-03 00:29:55 +0100

    mongodb-added

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.use(cors());
+app.use(express.json());
+
+
+const uri = "mongodb+srv://first-sever:first-server@cluster0.5432c9q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.log);
+
+
+app.get('/test', (req, res) => {
   res.send('Hello World!')
 })
 
